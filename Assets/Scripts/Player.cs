e63f4e2ad0e0d496e551c09e2003874e5fd7c3e9
Scott Slatton <scottslatton.tech@gmail.com>
2019-08-08 01:42:03 -0700

    added player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public CapsuleCollider playerCollider;
+    public float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCollider = this.GetComponent<CapsuleCollider>();
+        playerCollider.height = 1f;
+        playerCollider.center = new Vector3(0f,-.5f,0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveJump = Input.GetAxis("Jump");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveJump, moveVertical);
+
+
+        // moves the player character
+        transform.Translate(movement * Time.deltaTime * moveSpeed);
+
+        
+
+
+    }
+}
