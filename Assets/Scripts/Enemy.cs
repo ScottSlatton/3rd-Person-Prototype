ba7fbacaef21330274c40c04787cc71d747d96f9
Scott Slatton <scottslatton.tech@gmail.com>
2019-08-08 01:49:46 -0700

    added enemy script, can attack with right mouse button

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
+
 {
+
+    public int enemyHealth = 100;
+
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,12 +8,18 @@ public class Player : MonoBehaviour
     public CapsuleCollider playerCollider;
     public float moveSpeed = 5f;
 
+    private GameObject enemy;
+    private Enemy enemyScript;
+
     // Start is called before the first frame update
     void Start()
     {
         playerCollider = this.GetComponent<CapsuleCollider>();
         playerCollider.height = 1f;
         playerCollider.center = new Vector3(0f,-.5f,0f);
+
+        enemy = GameObject.Find("Battle_Dummy");
+        enemyScript = enemy.GetComponent<Enemy>();
     }
 
     // Update is called once per frame
@@ -30,8 +36,12 @@ public class Player : MonoBehaviour
         // moves the player character
         transform.Translate(movement * Time.deltaTime * moveSpeed);
 
-        
 
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            enemyScript.enemyHealth--;
+            print(enemyScript.enemyHealth);
+        }
 
     }
 }
